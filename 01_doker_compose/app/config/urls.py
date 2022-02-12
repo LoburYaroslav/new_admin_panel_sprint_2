a6ca6cbfc3f826d97a84dcd8c152ec3768a634cb
Lobur Yaroslav <lobur.iar@yandex.ru>
2022-02-12 20:47:21 +0300

    add django project

---
+++ 01_doker_compose/app/config/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+# from config.settings import DEBUG
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+# if DEBUG:
+#     urlpatterns.append(
+#         path('__debug__/', include('debug_toolbar.urls'))
+#     )
