97e895b4aee4eaaa62075b1d55aa382eb0622898
Lobur Yaroslav <lobur.iar@yandex.ru>
2022-02-15 22:13:52 +0300

    api v1

--- 01_doker_compose/app/config/urls.py
+++ 01_doker_compose/app/config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 # from config.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('movies.api.urls')),
+
 ]
 
 # if DEBUG:
