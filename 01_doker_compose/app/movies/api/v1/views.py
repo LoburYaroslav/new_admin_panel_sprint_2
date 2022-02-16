3e5e787244add38dc422668e548c23ec657c7e58
Lobur Yaroslav <lobur.iar@yandex.ru>
2022-02-16 20:18:49 +0300

    добавил апи

--- 01_doker_compose/app/movies/api/v1/views.py
+++ 01_doker_compose/app/movies/api/v1/views.py
@@ -1,10 +1,53 @@
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Q
 from django.http import JsonResponse
-from django.views import View
+from django.views.generic.detail import BaseDetailView
+from django.views.generic.list import BaseListView
+from movies.models import FilmWork
 
 
-class MoviesListApi(View):
+class MoviesApiMixin:
+    model = FilmWork
     http_method_names = ['get']
 
-    def get(self, request, *args, **kwargs):
-        # Получение и обработка данных
-        return JsonResponse({})
+    def get_queryset(self):
+        data = self.model.objects.values().annotate(
+            actors=ArrayAgg(
+                "persons__full_name", filter=Q(personfilmwork__role="actor")
+            ),
+            directors=ArrayAgg(
+                "persons__full_name", filter=Q(personfilmwork__role="director")
+            ),
+            writers=ArrayAgg(
+                "persons__full_name", filter=Q(personfilmwork__role="writer")
+            ),
+        )
+        return data
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class MoviesListApi(MoviesApiMixin, BaseListView):
+    paginate_by = 20
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        paginator, page, queryset, is_paginated = self.paginate_queryset(
+            self.get_queryset(),
+            self.paginate_by
+        )
+
+        context = {
+            'count': paginator.count,
+            'total_pages': paginator.num_pages,
+            "prev": page.previous_page_number() if page.has_previous() else None,
+            "next": page.next_page_number() if page.has_next() else None,
+            'results': list(queryset),
+        }
+        return context
+
+
+class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
+
+    def get_context_data(self, **kwargs):
+        return kwargs['object']
