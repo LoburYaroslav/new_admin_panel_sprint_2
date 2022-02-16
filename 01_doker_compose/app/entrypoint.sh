0bd3fad3d05141c609b15a7d81e3596944d1a242
Lobur Yaroslav <lobur.iar@yandex.ru>
2022-02-17 00:52:14 +0300

    gunicorn

--- 01_doker_compose/app/entrypoint.sh
+++ 01_doker_compose/app/entrypoint.sh
@@ -18,7 +18,7 @@ python3 manage.py migrate --noinput
 # some initialization code
 
 if [ $# == 0 ]; then
-  python3 manage.py runserver 0.0.0.0:8000
+  gunicorn config.wsgi:application --bind 0.0.0.0:8000
 else
   exec "$@"
 fi
